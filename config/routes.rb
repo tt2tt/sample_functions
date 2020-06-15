c84fd49918bb92112389c959791e74c59b129188
”taro <groveleffect@gmail.com>
2020-06-15 13:11:51 +0900

    MakeMailFunctionToDevelopment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   devise_for :users
-  root 'contacts#index' 
+  root 'contacts#index'
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
