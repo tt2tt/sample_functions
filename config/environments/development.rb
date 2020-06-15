54eb5004bfc1ff0ba5545f84a643c514f61f0230
”taro <groveleffect@gmail.com>
2020-06-15 14:53:39 +0900

    MakeMailFunction

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # Xserverでのメール設定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => ENV['SERVER'],
+    :port => 587,
+    :user_name => ENV['USERNAME'],
+    :password => ENV['PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
